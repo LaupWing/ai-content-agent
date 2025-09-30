5d741e605d2f6a2231e3a6b9835468403c1077f4
laupwing <laupwing@gmail.com>
2025-09-30 11:49:56 +0200

    Update macro_day_summary_agent to use api_diet_summary_today for daily macro summary retrieval

--- diet_coach/sub_agents/daily_summary/agent.py
+++ diet_coach/sub_agents/daily_summary/agent.py
@@ -1,10 +1,11 @@
 from google.adk.agents import Agent
 from . import prompts
+from diet_coach.tools import api_diet_summary_today
     
 macro_day_summary_agent = Agent(
     name="macro_day_summary_v1",
     model="gemini-2.5-flash",
     description="Retrieves the daily macro summary for the user.",
     instruction=prompts.MACRO_DAY_SUMMARY_PROMPT,
-    # tools=[macro_day_summary],  # wrap in FunctionTool if needed
+    tools=[api_diet_summary_today]
 )
