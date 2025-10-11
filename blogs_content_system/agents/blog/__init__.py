faa19dc3accb69a380a61e1358c02f2bdfba4f48
laupwing <laupwing@gmail.com>
2025-10-11 09:48:55 +0200

    Remove deprecated blog and headline agent modules along with their instructions

--- blogs_content_system/agents/blog/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Blog Writer Agent Module"""
-
-from .agent import blog_writer_agent
-from .prompt import BLOG_WRITER_INSTRUCTIONS
-
-__all__ = ['blog_writer_agent', 'BLOG_WRITER_INSTRUCTIONS']
--- blogs_content_system/agents/headline/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Headline Specialist Agent Module"""
-
-from .agent import headline_agent
-from .prompt import HEADLINE_AGENT_INSTRUCTIONS
-
-__all__ = ['headline_agent', 'HEADLINE_AGENT_INSTRUCTIONS']
