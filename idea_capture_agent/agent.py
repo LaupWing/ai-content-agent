69e765b718841378f69ea60ea928448e4c2b755b
laupwing <laupwing@gmail.com>
2025-10-16 08:58:25 +0200

    Update root_agent description and ensure add_idea agent is included in tools

--- idea_capture_agent/agent.py
+++ idea_capture_agent/agent.py
@@ -1,12 +1,14 @@
 from google.adk.agents import Agent
+from google.adk.tools import AgentTool
+from .agents.add_idea.agent import add_idea
 from . import prompt
 
 root_agent = Agent(
     name="idea_capture",
     model="gemini-2.5-flash",
     instruction=prompt.IDEA_CAPTURE_PROMPT,
-    description="",
+    description="You manage ideas in Notion: add, list, query, update, delete, expand, and report.",
     tools=[
-        
+        AgentTool(agent=add_idea)
     ]
 )
